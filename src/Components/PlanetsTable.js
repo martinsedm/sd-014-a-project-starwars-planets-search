b4e9337fbf40117e4cd8bd6dd2d18844435cd57a
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-10-30 09:03:57 -0300

    implementando tabela de planetas

---
+++ src/Components/PlanetsTable.js
@@ -0,0 +1,56 @@
+import React, { useContext } from 'react';
+
+import { PlanetState } from '../Context/PlanetProvider';
+
+const Table = () => {
+  const {
+    values: {
+      planetsToRender,
+    },
+  } = useContext(PlanetState);
+
+  return (
+    <main>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {planetsToRender.map((planet, i) => (
+            <tr key={ i }>
+              <td data-testid="planet-name">{planet.name}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.population}</td>
+              <td>{planet.films}</td>
+              <td>{planet.created}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.url}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
+};
+
+export default Table;
