4f45f4ec2be4057525e60cf57aad5c938bf842e9
tiagosathler <sathler@gmail.com>
2021-11-03 14:31:04 -0300

    Cria o componente Header para ser chamado dentro da page Home

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>StarWars Planets Search</h1>
+    </header>
+  );
+}
+
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Home() {
   return (
     <main>
+      <Header />
       <p>Hello world</p>
     </main>
   );
