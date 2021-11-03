e9a03795515e425d5d2102aafdec831fec229a0d
Flavio Pires <flavioasp77@gmail.com>
2021-11-03 17:10:43 -0300

    Criando a estrutura inicial da aplicaçao

---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+      <header>
+        <h1>Star WArs Planet Search</h1>
+      </header>
+      <main>
+        <section>
+          <input type="text" onChange="" value="" name="" className="input-text" />
+        </section>
+        <section>
+          <select>
+            <option>population</option>
+          </select>
+          <select>
+            <option>menor que</option>
+          </select>
+          <input type="number" onChange="" value="" name="" className="input-text" />
+          <button type="button" onClick="">Filtrar</button>
+        </section>
+        <table>
+          <thead />
+        </table>
+      </main>
+    </>
+  );
+}
+
+export default Home;
