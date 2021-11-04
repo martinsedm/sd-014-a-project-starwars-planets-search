d56dd6f496c6bffd6e5e71744ef519f32df9ada9
Heitor SF <heitor.sf5@gmail.com>
2021-11-04 19:47:48 -0300

    Bug not fixed, but mitigated. Will leave as is with my comments for future reference

--- src/FilterField.jsx
+++ src/FilterField.jsx
@@ -3,7 +3,7 @@ import PlanetsContext from './context/PlanetsContext';
 
 function FilterField() {
   const { getNameFilter, getValueFilter } = useContext(PlanetsContext);
-  const [numFilter, setNumFilter] = useState({}); // THERE'S A PECULIARITY HERE***
+  const [numFilter, setNumFilter] = useState({}); // THERE'S A PECULIARITY HERE *** MORE AT THE END OF THE PAGE
   const [spentFilters, setSpentFilters] = useState({
     population: false,
     orbital_period: false,
@@ -12,7 +12,8 @@ function FilterField() {
     surface_water: false,
   });
   // const firstUnspentFilter = Object.keys(spentFilters).find((elem) => !spentFilters[elem]);
-  // const [numFilter, setNumFilter] = useState({ column: firstUnspentFilter });
+  // const [firstAbleFilter, setAbleFilter] = useState(Object.keys(spentFilters).find((elem) => !spentFilters[elem]));
+  // const [numFilter, setNumFilter] = useState({ column: firstAbleFilter });
 
   const handleChange = ({ target }) => {
     setNumFilter({
@@ -20,6 +21,12 @@ function FilterField() {
       [target.name]: target.value,
     });
   };
+
+  // const updateSpentFilters = async () => {
+  //  await setAbleFilter(Object.keys(spentFilters).find((elem) => !spentFilters[elem]));
+  //   console.log(spentFilters);
+  //  await setNumFilter({ column: firstAbleFilter });
+  // };
 
   const submitFilter = () => {
     const { column, comparison, value } = numFilter;
@@ -31,6 +38,7 @@ function FilterField() {
         ...spentFilters,
         [column]: true,
       });
+      // updateSpentFilters();
     }
   };
 
@@ -38,11 +46,12 @@ function FilterField() {
     const optionFilter = Object.keys(spentFilters).filter((columnName) => (
       !spentFilters[columnName]
     ));
-    const columnRender = optionFilter.map((name) => (
+    const columnRender = optionFilter.map((name, index) => (
       <option
         key={ name }
         value={ name }
         disabled={ spentFilters[name] }
+        selected={ index === 0 }
       >
         {name}
       </option>));
@@ -50,7 +59,6 @@ function FilterField() {
   };
 
   useEffect(() => {
-    columnOptions();
   });
 
   return (
@@ -102,4 +110,4 @@ function FilterField() {
 
 export default FilterField;
 
-// **** OBSERVATION: Because the tests from req 4 WANT to see exactly 4 choices after spending one filter, I can't use my placeholder. Which means that in the app you'll either have to reselect the column if you want the default (e.g. population, if it's not spent), or you'll end up with a bug that keeps counting it as population if I don't ever choose anything else, despite it being disabled.
+// **** OBSERVATION: Because the tests from req 4 WANT to see exactly 4 choices after spending one filter, I can't use my placeholder, or it will count as one more than it should everytime. Which means that in the app you'll either have to reselect the column if you want the default (e.g. population, if it's not spent), or you'll end up with a bug that keeps counting it as population if I don't ever choose anything else, despite it being disabled by that point.
