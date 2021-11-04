0b581c319e880e1631cb7b262a0ff9fd5b47154c
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-11-04 17:01:05 -0300

    R3-R5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Provider from './context/Provider';
 import FilterName from './components/FilterName';
-// import FilterNumber from './components/FilterNumber';
 import Table from './components/Table';
-// import Clearfilters from './components/Clearfilters';
+import FilterNumber from './components/FilterNumber';
+import Clearfilters from './components/Clearfilters';
 
 function App() {
   return (
     <Provider>
       <FilterName />
-      {/* <FilterNumber /> */}
+      <FilterNumber />
+      <Clearfilters />
       <Table />
-      {/* <Clearfilters /> */}
     </Provider>
   );
 }
