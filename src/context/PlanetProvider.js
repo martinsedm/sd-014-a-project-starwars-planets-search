4d0184d489510c4332e990c4c65ce06dec8c181d
brunofeu <brunofeu89@gmail.com>
2021-11-04 18:48:41 -0300

    requirement 02 ok

--- src/context/PlanetProvider.js
+++ src/context/PlanetProvider.js
@@ -6,11 +6,14 @@ import PlanetContext from './PlanetContext';
 function PlanetProvider({ children }) {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [filteredPlanets, setFilteredPlanets] = useState([]);
+  const [filterByText, setFilterByText] = useState('');
 
   async function fetchPlanetsList() {
     setIsLoading(true);
     const dataPlanets = await getPlanets();
     setData(dataPlanets);
+    setFilteredPlanets(dataPlanets);
     setIsLoading(false);
   }
 
@@ -18,8 +21,17 @@ function PlanetProvider({ children }) {
     fetchPlanetsList();
   }, []);
 
+  useEffect(() => {
+    const filtered = data.filter(({ name }) => name.includes(filterByText));
+    setFilteredPlanets(filtered);
+  }, [data, filterByText]);
+
   return (
-    <PlanetContext.Provider value={ { data, isLoading } }>
+    <PlanetContext.Provider
+      value={
+        { data, isLoading, filteredPlanets, filterByText, setFilterByText }
+      }
+    >
       {children}
     </PlanetContext.Provider>
   );
