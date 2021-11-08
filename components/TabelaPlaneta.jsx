329bd1da061e79d752c76c28f4531ba7326ec386
Veruska de Oliveira <veruska.ideasdigital@gmail.com>
2021-11-08 22:09:58 +0000

    Filtro Tabela ESLint

--- components/TabelaPlaneta.jsx
+++ components/TabelaPlaneta.jsx
@@ -1,37 +1,40 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
-import ContextTabela from '../context/ContextTabela';
-import FiltroNome from '../components/FiltroNome';
+import ContextTabela from "../context/ContextTabela";
+import FiltroNome from "./FiltroNome";
 
 function TabelaPlaneta() {
   const { data, titles, filterName } = useContext(ContextTabela);
 
   return (
     <section>
-        <FiltroNome />
+      <FiltroNome />
       <table border="1">
         <thead>
           <tr>
-            {titles.map((title) => <th key={ title }>{title}</th>)}
+            {titles.map((title) => (
+              <th key={ title }>{ title }</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-        {data.filter((planet) => (planet.name.includes(filterName)))
+          {data
+            .filter((planet) => planet.name.includes(filterName))
             .map((planets, index) => (
               <tr key={ index }>
-                <td>{planets.name}</td>
-                <td>{planets.rotation_period}</td>
-                <td>{planets.orbital_period}</td>
-                <td>{planets.diameter}</td>
-                <td>{planets.climate}</td>
-                <td>{planets.gravity}</td>
-                <td>{planets.terrain}</td>
-                <td>{planets.surface_water}</td>
-                <td>{planets.population}</td>
-                <td>{planets.films}</td>
-                <td>{planets.created}</td>
-                <td>{planets.edited}</td>
-                <td>{planets.url}</td>
+                <td>{ planets.name }</td>
+                <td>{ planets.rotation_period }</td>
+                <td>{ planets.orbital_period }</td>
+                <td>{ planets.diameter }</td>
+                <td>{ planets.climate }</td>
+                <td>{ planets.gravity }</td>
+                <td>{ planets.terrain }</td>
+                <td>{ planets.surface_water }</td>
+                <td>{ planets.population }</td>
+                <td>{ planets.films }</td>
+                <td>{ planets.created }</td>
+                <td>{ planets.edited }</td>
+                <td>{ planets.url }</td>
               </tr>
             ))}
         </tbody>
