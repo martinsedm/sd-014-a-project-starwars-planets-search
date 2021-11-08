f6aea5c051f6b466e67dd028b7f0815f14d0cebb
Dayane <dayanebmartins@gmail.com>
2021-11-07 21:41:13 -0300

    cria componete removeFilter

---
+++ src/Components/RemoveFilter.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import PlanetContext from '../context/PlanetContext';
+
+function RemoveFilter() {
+  const { removeFilterColumn, filters } = useContext(PlanetContext);
+  const { filterByNumericValues } = filters;
+  return (
+    <div>
+      <section>
+        {filterByNumericValues.map(
+          ({ column, comparison, value }) => (
+            <div key={ column }>
+              <p>
+                {column}
+                {comparison}
+                {value}
+              </p>
+              <button
+                type="button"
+                onClick={ () => removeFilterColumn(column) }
+              >
+                X
+              </button>
+            </div>
+          ),
+        )}
+      </section>
+    </div>
+  );
+}
+
+export default RemoveFilter;
