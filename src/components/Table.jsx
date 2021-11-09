612863b92bb684a6fefe44f72f6f31479cf9583a
Lucas Freitas <lucasr.freitas@icloud.com>
2021-11-09 12:45:51 -0300

    finaliza tabela

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -16,6 +16,27 @@ export default function Table() {
             }
           </tr>
         </thead>
+        <tbody>
+          {
+            planets.map((planet, index) => (
+              <tr key={ index.name }>
+                <td>{ planet.name}</td>
+                <td>{ planet.rotation_period }</td>
+                <td>{ planet.orbital_period }</td>
+                <td>{ planet.diameter }</td>
+                <td>{ planet.climate }</td>
+                <td>{ planet.gravity }</td>
+                <td>{ planet.terrain }</td>
+                <td>{ planet.surface_water }</td>
+                <td>{ planet.population }</td>
+                <td>{ planet.films }</td>
+                <td>{ planet.created }</td>
+                <td>{ planet.edited }</td>
+                <td>{ planet.url }</td>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     </div>
   );
