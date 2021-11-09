f3d058af1bef38256a507b2d890b2c0ca1c15eb6
MartinSilva <martinempreendedor@gmail.com>
2021-11-09 15:33:18 -0300

    item 3. parte 1

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -14,15 +14,52 @@ function Filter() {
 
   return (
     <div>
-      Filter
+      <div>
+        Filter
+        <input
+          type="text"
+          data-testid="name-filter"
+          value={ filters.filterByName.name }
+          onChange={ handleChange }
+          placeholder="Filtre pelo nome"
+        />
+      </div>
+      <select
+        data-testid="column-filter"
+        // onChange={ }
+        // value={  }
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+      <select
+        data-testid="comparison-filter"
+        // onChange={  }
+        // vale={  }
+      >
+        <option value="maior que">maior que</option>
+        <option value="igual a">igual a</option>
+        <option value="menor que">menor que</option>
+      </select>
       <input
-        type="text"
-        data-testid="name-filter"
-        value={ filters.filterByName.name }
-        onChange={ handleChange }
-        placeholder="Filtre pelo nome"
+        data-testid="value-filter"
+        type="number"
+        // onChange={  }
+        // value={  }
       />
+
+      <button
+        type="button"
+        data-testid="button-filter"
+        // onClick={ }
+      >
+        Filtrar
+      </button>
     </div>
+
   );
 }
 
