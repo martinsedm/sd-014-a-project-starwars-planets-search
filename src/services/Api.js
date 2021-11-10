9e75e87b618ecf465f67fe1019ba8a8abc9245f9
Diogo Bontempo <diogo_bontempo@hotmail.com>
2021-11-10 13:02:17 -0200

    inicio

---
+++ src/services/Api.js
@@ -0,0 +1,15 @@
+async function Api() {
+  const request = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+
+  const jsonResponse = await request.json();
+
+  const planets = jsonResponse.results.map((planet) => {
+    const { residents, ...othersProps } = planet;
+
+    return { ...othersProps };
+  });
+
+  return planets;
+}
+
+export default Api;
