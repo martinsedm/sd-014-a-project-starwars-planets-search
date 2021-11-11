9ba7a02532d34faee6df064992b044f4ea77da21
BeatrizR <biarl.ramalhorl@gmail.com>
2021-11-11 12:42:56 -0300

    agora vai

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Context from '../context/Context';
 import TableInfos from './TableInfos';
 import { dropdownColumns, dropdownTag } from '../data/data';
