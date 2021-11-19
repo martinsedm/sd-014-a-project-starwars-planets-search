a8b43f8e95699fc37901c8807869b5bc546daba2
Isaddora Bayma <isadora.bayma@gmail.com>
2021-11-18 23:13:19 -0300

    req 1 - creating a table from API data

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,36 +1,56 @@
-import React, { useEffect } from 'react';
-// import Context from '../contexts/Context';
+import React, { useEffect, useState } from 'react';
 
 export default function Table() {
-  // const planets = useContext(Context);
+  const [planets, setPlanets] = useState([]);
 
-  // let planets = [];
-  // planets = fetchByPlanets;
-  // console.log(planets);
   const fecthPlanets = async () => {
     const URL = 'https://swapi-trybe.herokuapp.com/api/planets/';
     const response = await fetch(URL);
     const json = await response.json();
-    console.log(json);
+    setPlanets(json.results);
   };
 
   useEffect(() => {
     fecthPlanets();
   }, []);
 
+  if (planets.length === 0) return <span>Loading...</span>;
+
   return (
     <table>
       <tr>
-        xxx
+        <th>Name</th>
+        <th>Rotation Period</th>
+        <th>Orbital Period</th>
+        <th>Diameter</th>
+        <th>Climate</th>
+        <th>Gravity</th>
+        <th>Terrain</th>
+        <th>Surface Water</th>
+        <th>Population</th>
+        <th>Films</th>
+        <th>Created</th>
+        <th>Edited</th>
+        <th>URL</th>
       </tr>
+      { planets.map((planet) => {
+        // const URL_BASE = 'https://swapi-trybe.herokuapp.com/api/planets/';
+        // const URLPlanet = planet.URl;
+        // const id = URLPlanet.replace(URL_BASE, '');
+        return (
+          <tr key={ planet.URL }>
+            <td>{planet.name}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.population}</td>
+          </tr>
+        );
+      })}
     </table>
-    // { xxx.map(() => {
-    //   return (
-    //     <tr key= >
-    //       <td></td>
-    //     </tr>
-    //   )
-    // })
-    // }
   );
 }
