549f3a82372685870424134c30de8d5df229e391
DanFilipe2000 <anielilipe90@gmail.com>
2021-11-24 22:36:51 -0300

    Requisito 4

--- src/Componets/PlanetsList.jsx
+++ src/Componets/PlanetsList.jsx
@@ -7,6 +7,39 @@ export default function PlanetsList() {
   const { data } = useContext(PlanetsContext);
   const [filterPlanets, setPlanets] = useState(data);
   const [searchText, setSearchText] = useState('');
+  const [numericFilters, setNumFilter] = useState([]);
+
+  const defaultSelects = [
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ];
+
+  const [selects, setSelects] = useState(defaultSelects);
+  const [selectFilters, setSelectFilters] = useState([]);
+
+  const setFilters = () => {
+    const column = document.getElementById('column').value;
+    const comparison = document.getElementById('comparison').value;
+    const number = document.getElementById('value').value;
+
+    const filtros = [
+      ...numericFilters,
+      {
+        column,
+        comparison,
+        number,
+      },
+    ];
+
+    const modifySelects = selects.filter((e) => e !== column);
+
+    setSelectFilters(filtros);
+    setNumFilter(filtros);
+    setSelects(modifySelects);
+  };
 
   const renderInputText = () => (
     <input
@@ -14,6 +47,37 @@ export default function PlanetsList() {
       type="text"
       onChange={ ({ target }) => setSearchText(target.value) }
     />
+  );
+
+  const renderNumericFiltersForm = () => (
+    <form>
+      <select
+        id="column"
+        data-testid="column-filter"
+      >
+        { selects.map((e, index) => <option key={ index } value={ e }>{ e }</option>)}
+      </select>
+      <select
+        id="comparison"
+        data-testid="comparison-filter"
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <input
+        id="value"
+        type="number"
+        data-testid="value-filter"
+      />
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ () => setFilters() }
+      >
+        Filtrar
+      </button>
+    </form>
   );
 
   useEffect(() => {
@@ -24,9 +88,21 @@ export default function PlanetsList() {
     planetsFilter(searchText);
   }, [searchText, data]);
 
+  useEffect(() => {
+  }, []);
+
   return (
     <main>
       { renderInputText() }
+      { renderNumericFiltersForm() }
+      {
+        selectFilters.map(({ column, comparison, number }, index) => (
+          <section key={ index } id={ column }>
+            <p>{`${column} ${comparison} ${number}`}</p>
+            <button type="button" value={ column }>X</button>
+          </section>
+        ))
+      }
       <table>
         <tr>
           <th>Name</th>
