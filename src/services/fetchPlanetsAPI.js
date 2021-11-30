f412a7d31b37d4e467816ffb6b2efe192dc414cb
PeDD1 <pedrohr0909@gmail.com>
2021-11-29 23:12:13 -0300

    Change forEach in FetchApi to map

--- src/services/fetchPlanetsAPI.js
+++ src/services/fetchPlanetsAPI.js
@@ -4,7 +4,7 @@ const fetchPlanetsAPI = async () => {
   const request = await fetch(END_POINT);
   const response = await request.json();
   const { results } = response;
-  results.forEach((planet) => delete planet.residents);
+  results.map((planet) => delete planet.residents);
   return results;
 };
 
