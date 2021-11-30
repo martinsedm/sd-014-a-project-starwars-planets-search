0ab04152f50665d53efc1e87fe1ee0ca4b451a0e
Lua <luabaronto@hotmail.com>
2021-11-29 23:42:36 -0300

    correacao lint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-
 import React from 'react';
-import './App.css';
 import Table from './Components/Table';
 import { Provider } from './context/Provider';
 import Filters from './Components/Filters';
 
 function App() {
   return (
-    <span>Hello, App!</span>
     <Provider>
       <Filters />
       <Table />
